4c6fd94b996441ada60cfa54a2c0984f1ee8b1ce
Үүрцайх Норовсамбуу <you@example.com>
2024-01-07 19:44:12 -0800

    Jan8M

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Card from "./components/Card";
+
+export default function App() {
+  return (
+    <div>
+      <p>Hello World</p>
+      <Card />
+      <Card />
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import App from "./App";
+import ReactDom from "react-dom";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+//file iin ner tomoor yvna index.js ees busadn
+//anhnaasaa tomooo=r bicheh
+//adiilhan nertei function baina
